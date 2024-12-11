324dad95fdc78e7acea73539bddc19d0a86a77c8
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-12-11 12:09:08 +0700

    config: add Playwright extension support and configuration updates

---
+++ playwright/commands/custom-gencode.ts
@@ -0,0 +1,41 @@
+const { chromium } = require("@playwright/test");
+const { exec } = require("child_process");
+const path = require("path");
+
+(async () => {
+  const pathToExtension = path.resolve(
+    "/Users/phakh/Library/Application Support/Google/Chrome/Default/Extensions/ophjlpahpchlmihnnnihgmmeilfjmjjc/3.6.0_0"
+  );
+  const context = await chromium.launchPersistentContext("", {
+    headless: false,
+    args: [
+      `--disable-extensions-except=${pathToExtension}`,
+      `--load-extension=${pathToExtension}`,
+    ],
+  });
+
+    const page = await context.newPage();
+    await page.goto(
+      "chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html"
+    );
+
+  // Close the browser context to save the storage state
+  //   await context.close();
+  //   await context.storageState({ path: "auth.json" });
+
+  // Start the codegen process
+  exec(
+    "npx playwright codegen chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html --save-storage=auth.json",
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Stderr: ${stderr}`);
+        return;
+      }
+      console.log(`Stdout: ${stdout}`);
+    }
+  );
+})();
--- playwright/supports/fixtures.ts
+++ playwright/supports/fixtures.ts
@@ -13,7 +13,7 @@ export const test = base.extend<{
 }>({
   context: async ({}, use) => {
     const pathToExtension =
-      "/Users/phakh/Library/Application Support/Google/Chrome/Default/Extensions/ophjlpahpchlmihnnnihgmmeilfjmjjc/3.6.0_0";
+      "/Users/phakh/Library/Application Support/Google/Chrome/Default/Extensions/ophjlpahpchlmihnnnihgmmeilfjmjjc/3.6.1_0";
     const context = await chromium.launchPersistentContext("", {
       headless: false,
       args: [
@@ -25,15 +25,8 @@ export const test = base.extend<{
     await context.close();
   },
   backgroundPage: async ({ context }, use) => {
-    // for manifest v2:
-    // let [background] = context.backgroundPages()
-    // if (!background)
-    //   background = await context.waitForEvent('backgroundpage')
-
-    // for manifest v3:
     let [background] = context.serviceWorkers();
     if (!background) background = await context.waitForEvent("serviceworker");
-
     await use(background);
   },
   extensionId: async ({ backgroundPage }, use) => {
